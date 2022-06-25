acd3a80c7e915d2fd111d1cc0bb9d86779bdb78b
Raz Friman <raz@canva.com>
2022-06-26 09:02:19 +1000

    Address lighthouse

--- src/ui/AppFooter.tsx
+++ src/ui/AppFooter.tsx
@@ -9,21 +9,21 @@ export const AppFooter = () => {
       justifyContent: "center"
     }}>
       <Tooltip label="LinkedIn" allowPointerEvents={true}>
-        <ActionIcon component="a" href="https://www.linkedin.com/in/razfriman"
+        <ActionIcon component="a" href="https://www.linkedin.com/in/razfriman" aria-label="LinkedIn link"
           target="_blank">
           <Icon icon="linkedin" />
         </ActionIcon>
       </Tooltip>
 
       <Tooltip label="GitHub" allowPointerEvents={true}>
-        <ActionIcon component="a" href="https://github.com/razfriman"
+        <ActionIcon component="a" href="https://github.com/razfriman" aria-label="Github link"
           target="_blank">
           <Icon icon="github" />
         </ActionIcon>
       </Tooltip>
 
       <Tooltip label="Email" allowPointerEvents={true}>
-        <ActionIcon component="a" href="mailto:raz.friman@razfriman.com"
+        <ActionIcon component="a" href="mailto:raz.friman@razfriman.com" aria-label="Email link"
           target="_blank">
           <Icon icon="email" />
         </ActionIcon>
--- src/ui/ThemeSwitcher.tsx
+++ src/ui/ThemeSwitcher.tsx
@@ -4,7 +4,7 @@ import { Icon } from "./Icon";
 export const ThemeSwitcher = () => {
     const { colorScheme, toggleColorScheme } = useMantineColorScheme();
     return (
-        <ActionIcon onClick={() => toggleColorScheme()}>
+        <ActionIcon onClick={() => toggleColorScheme()} aria-label="switch theme button">
             <Icon icon={colorScheme === 'dark' ? 'sun' : 'moon'} />
         </ActionIcon>
     );
