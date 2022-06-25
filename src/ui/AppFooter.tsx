67b0df4d8b517e73a896e4e045914b7e03c6a7dc
Raz Friman <raz@canva.com>
2022-06-25 18:50:04 +1000

    footer

--- src/ui/AppFooter.tsx
+++ src/ui/AppFooter.tsx
@@ -3,32 +3,34 @@ import { Icon } from "./Icon";
 
 export const AppFooter = () => {
   return (
-    <Box>
-      <Box>
-        <Tooltip label="LinkedIn">
-          <ActionIcon component="a" href="https://www.linkedin.com/in/razfriman"
-            target="_blank"
-            sx={{ color: "#007bb6" }}>
-            <Icon icon="linkedin" />
-          </ActionIcon>
-        </Tooltip>
+    <Box sx={{
+      gap: "20px",
+      display: "flex",
+      justifyContent: "center"
+    }}>
+      <Tooltip label="LinkedIn">
+        <ActionIcon component="a" href="https://www.linkedin.com/in/razfriman"
+          target="_blank"
+          sx={{ color: "#007bb6" }}>
+          <Icon icon="linkedin" />
+        </ActionIcon>
+      </Tooltip>
 
-        <Tooltip label="GitHub">
-          <ActionIcon component="a" href="https://github.com/razfriman"
-            target="_blank"
-            sx={{ color: "#000000" }}>
-            <Icon icon="github" />
-          </ActionIcon>
-        </Tooltip>
+      <Tooltip label="GitHub">
+        <ActionIcon component="a" href="https://github.com/razfriman"
+          target="_blank"
+          sx={{ color: "#000000" }}>
+          <Icon icon="github" />
+        </ActionIcon>
+      </Tooltip>
 
-        <Tooltip label="Email">
-          <ActionIcon component="a" href="mailto:raz.friman@razfriman.com"
-            target="_blank"
-            sx={{ color: "#a9a9a9" }}>
-            <Icon icon="email" />
-          </ActionIcon>
-        </Tooltip>
-      </Box>
+      <Tooltip label="Email">
+        <ActionIcon component="a" href="mailto:raz.friman@razfriman.com"
+          target="_blank"
+          sx={{ color: "#a9a9a9" }}>
+          <Icon icon="email" />
+        </ActionIcon>
+      </Tooltip>
     </Box>
   );
 };
