8259842c1298e0dc07d3115ccef1e1c0e11a9ad2
Raz Friman <raz@canva.com>
2022-06-26 11:30:33 +1000

    install prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,32 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "@typescript-eslint", "prettier"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-      "react/react-in-jsx-scope": "off",
+    "import/resolver": {
+      typescript: {},
     },
-    "settings": {
-      "react": {
-        "version": "detect"
-      }
-    }
-}
+  },
+};
--- src/ui/AppHeader.tsx
+++ src/ui/AppHeader.tsx
@@ -131,3 +131,5 @@ export const AppHeader = () => {
         </Header>
     );
 }
+
+
