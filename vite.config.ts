b8809454aa19dde3ca18166372ef356847bc16c4
Raz Friman <raz@canva.com>
2023-08-19 12:32:56 +1000

    reset

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import react from '@vitejs/plugin-react';
-import * as path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-});
+})
