719f60c24250c450d4d5154bbfa71a6260776035
Raz Friman <raz@canva.com>
2023-08-19 12:41:40 +1000

    shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import reactLogo from './assets/react.svg'
+import { Button } from './components/ui/button'
 import viteLogo from '/icon.svg'
 
 function App() {
@@ -20,9 +21,9 @@ function App() {
       React + Tailwind
       </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
